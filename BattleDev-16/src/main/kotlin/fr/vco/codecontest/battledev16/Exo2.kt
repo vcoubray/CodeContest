cfbfabc98d6c3585a480d3cf28e7f38ec4dbdc9d
vcoubray <vcoubray@sedona.fr>
2020-11-26 20:46:27 +0100

    BattleDev#16 : Exo2

--- BattleDev-16/src/main/kotlin/fr/vco/codecontest/battledev16/Exo2.kt
+++ BattleDev-16/src/main/kotlin/fr/vco/codecontest/battledev16/Exo2.kt
@@ -4,7 +4,12 @@ object Exo2 {
 
     // Copy only the main method in the Isograd platform
     fun main() {
+        val input = generateSequence(::readLine).toList()
+        val lines = input.subList(1,input.size)
 
+        val (night, day) = lines.map{it.substring(0,2).toInt()}.partition{it >= 20 || it <= 7 }
+        val sus = if (night.size > day.size) "SUSPICIOUS" else "OK"
+        println(sus)
 
 
     }
